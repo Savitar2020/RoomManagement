7eb2d4ec79f9b63b62ff825ac507b9a92c2bc4d5
goncalvesmBZZ <goncalvesm@bzz.ch>
2021-12-02 10:16:38 +0100

    services and config changes

--- src/main/java/roommanagement/service/Config.java
+++ src/main/java/roommanagement/service/Config.java
@@ -10,11 +10,11 @@ import java.util.Properties;
 import java.util.Set;
 
 /**
- * configure the web services and properties
+ * services for the projects
  * <p>
- * M151: BookDB
+ * 426: Scrum-Project
  *
- * @author Marcel Suter (Ghwalin)
+ * @author Marcio Duarte
  */
 
 @ApplicationPath("/resource")
@@ -31,9 +31,9 @@ public class Config extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         HashSet providers = new HashSet<Class<?>>();
-//        providers.add(BookService.class);
-//        providers.add(UserService.class);
-//        providers.add(PublisherService.class);
+        providers.add(EventService.class);
+        providers.add(ReservationService.class);
+        providers.add(RoomService.class);
         return providers;
     }
 
