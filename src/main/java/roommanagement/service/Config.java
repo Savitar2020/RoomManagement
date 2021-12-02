05484697af321baac6c49cfb1616dc290d0884f8
goncalvesmBZZ <goncalvesm@bzz.ch>
2021-12-02 09:07:15 +0100

    push for maven

--- src/main/java/roommanagement/service/Config.java
+++ src/main/java/roommanagement/service/Config.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @ApplicationPath("/resource")
 
 public class Config extends Application {
-    private static final String PROPERTIES_PATH = "/home/bzz/webapp/bookDB.properties";
+    private static final String PROPERTIES_PATH = "/home/bzz/webapp/roomManagmentDB.properties";
     private static Properties properties = null;
 
     /**
