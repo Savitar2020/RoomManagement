58eb4641e7ea9f32e45b36bb03ebcd3ad1df384e
bzz <Savitar2020>
2022-01-06 10:31:26 +0100

    login.html mostly done Done

---
+++ out/artifacts/RoomManagement_war_exploded/js/loginValidation.js
@@ -0,0 +1,10 @@
+function validation() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "Hauswart" && password === "Password") {
+        window.open("reservationsliste.html")
+    } else {
+        alert("Fuck You");
+    }
+}
