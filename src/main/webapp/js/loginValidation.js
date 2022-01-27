0ef441caddd02b2da6805d562b819738f0de53dd
bzz <Savitar2020>
2022-01-27 08:51:28 +0100

    Date added to detailpage_Reservation.html

--- src/main/webapp/js/loginValidation.js
+++ src/main/webapp/js/loginValidation.js
@@ -3,7 +3,9 @@ function validation() {
     const password = document.getElementById("password").value;
 
     if (username === "Hauswart" && password === "Password") {
-        window.open("eventliste.html");
+        window.open("eventliste.html")
+    } else if (username === "Admin" && password === "Password") {
+        window.open("reservationsliste.html")
     } else {
         alert("Benutername oder Passwort falsch");
     }
